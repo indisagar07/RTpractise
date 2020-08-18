9b2f8ec0066d295240631d3ea271113589704d14
baale <baalelk@gmail.com>
2020-08-18 12:56:08 +0530

    pull andpush sucess

---
+++ src/First.java
@@ -0,0 +1,9 @@
+package com.baalefirst
+
+class First
+{
+	public static void main(String[] args)
+	{
+		system.out.println("this is first commit");
+	}
+}
---
+++ src/Second.java
@@ -0,0 +1,10 @@
+package com.baalefirst
+
+class Second
+{
+	pubic static void main(String[] args)
+	{
+		System.out.println("this is second file");
+	}
+
+}
